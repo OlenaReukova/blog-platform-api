c2f76f32c8b84e8af25bd6409eb694734c26f6ce
elena <kronverk01@gmail.com>
2026-04-21 23:53:40 +0100

    test: add unit tests for PostService.getAllPosts()

---
+++ src/test/java/org/example/blogplatform/service/PostServiceTest.java
@@ -0,0 +1,64 @@
+package org.example.blogplatform.service;
+
+import org.example.blogplatform.model.Post;
+import org.example.blogplatform.repository.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+    // creates a FAKE postRepository
+    @Mock
+    private PostRepository postRepository;
+
+
+    // creates a REAL PostService but injects the fake repository into it
+    @InjectMocks
+    private PostService postService;
+
+    @Test
+    void shouldReturnAllPosts() {
+        //Given
+        Post post1 = new Post();
+        post1.setTitle("First Post");
+
+        Post post2 = new Post();
+        post2.setTitle("Second Post");
+
+        //when findAll() is called, return these 2 posts
+        when(postRepository.findAll()).thenReturn(List.of(post1, post2));
+
+        // WHEN — call the actual method you are testing
+        List<Post> result = postService.getAllPosts();
+
+        // THEN — check the result is what you expect
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).getTitle()).isEqualTo("First Post");
+        verify(postRepository, times(1)).findAll();
+        // verify() checks that findAll() was called exactly once
+        // confirms your service actually used the repository
+
+    }
+    @Test
+    void shouldReturnEmptyListWhenNoPostsExist() {
+        // given
+        when(postRepository.findAll()).thenReturn(List.of());
+
+        // when
+        List<Post> result = postService.getAllPosts();
+
+        // then
+        assertThat(result).isEmpty();
+        verify(postRepository, times(1)).findAll();
+    }
+
+}
