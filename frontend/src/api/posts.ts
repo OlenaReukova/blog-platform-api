59bce3d3e438858df5682bb98a61d9911cea220a
elena <kronverk01@gmail.com>
2026-04-23 20:13:01 +0100

    Add createPost method

--- frontend/src/api/posts.ts
+++ frontend/src/api/posts.ts
@@ -6,4 +6,14 @@ const API_BASE = 'http://localhost:8080/api/posts';
 export const getAllPosts = async (): Promise<Post[]> => {
     const response = await axios.get<Post[]>(API_BASE);
     return response.data;
+
+    export const createPost = async (post: {
+        title: string;
+        content: string;
+        author: string;
+        tags: string[];
+    }):Promise<Post> => {
+       const response = await axios.post<Post>(API_BASE, post);
+       return response.data;
+    }
 };
